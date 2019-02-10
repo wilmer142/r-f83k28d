8644915a2de49f13e49f360f4e1136a82b3c9774
wilmer142 <wilmercollege@gmail.com>
2019-02-10 14:00:01 -0500

    Solve challenge 2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	constructor(){
+		super();
+
+		this.state = {
+			number: 0
+		}
+	}
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.number}</span>
+        <button onClick={this.incrementNumber.bind(this)} id="inc">Incrementa</button>
       </div>
     );
+  }
+
+  incrementNumber(){
+
+  	this.setState({
+  		number: (this.state.number + 1)
+  	});
   }
 }
 
